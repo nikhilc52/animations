e841985f79de648dc97c7eb57a28471b45ade2e5
Nikhil Chinchalkar <147773549+nikhilc52@users.noreply.github.com>
2024-06-07 11:44:59 -0400

    added NYT headline animations and files

---
+++ headlines.R
@@ -0,0 +1,60 @@
+library(tidyverse)
+library(dplyr)
+library(gganimate)
+library(ggwordcloud)
+library(stopwords)
+
+headlines <- read_csv("headlines.csv")
+
+nyt_headlines <- headlines |> 
+  filter(Publication == "New York Times")
+
+nyt_headlines$Date <- as.Date(as.character(nyt_headlines$Date), format="%Y%m%d")
+
+nyt_headlines <- nyt_headlines |> 
+  filter(!is.na(Headline)) |> 
+  mutate(Year = format(Date, "%Y"))
+
+nyt_headlines <- nyt_headlines |> 
+  group_by(Year) |> 
+  summarize(
+    Headlines = paste(Headline, collapse = " "), 
+    Words = unlist(strsplit(Headlines, split = " "))
+  )
+
+nyt_headlines <- nyt_headlines[-2]
+
+word_count <- nyt_headlines |> 
+  group_by(Words, Year) |> 
+  summarize(
+    count = n()
+  ) |> 
+  filter(!(tolower(Words) %in% stopwords(language="en", source="snowball")))
+
+word_count <- word_count |> 
+  filter(!(tolower(Words) %in% c("new", "york", "times", "nyt"))) |> 
+  filter(!tolower(Words) %in% c("min", "read", "review", "editorial", "now",
+                                "comments", "getty", "images", "theater",
+                                "movie", "slide", "show", "television", "books")) |> 
+  filter(!grepl("[^A-Za-z0-9 ]", Words)) |> 
+  filter(!(Words == "")) |> 
+  filter(count > 10) |> 
+  group_by(Year) |> 
+  arrange(desc(count)) |> 
+  slice_head(n = 10)
+
+word_count$Year <- as.numeric(word_count$Year)
+
+word_cloud <- word_count |> 
+  ggplot(aes(label=Words, size=count))+
+  geom_text_wordcloud()+
+  theme_minimal()+
+  scale_size_area(max_size = 30)+
+  transition_time(Year)+
+  labs(subtitle = "Year: {frame_time}",
+       title = "Top Words Of NYT Headlines")+
+  theme(plot.title = element_text(size=16, hjust=0.5),
+        plot.subtitle = element_text(size=14, hjust=0.5))
+
+animate(word_cloud, fps = 1, duration = 19, end_pause = 3, height = 7,
+        width = 7, units = "in", res = 200)
