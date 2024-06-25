030016d109bc2379ce23cc573216e0fbadb8f3c5
Nikhil Chinchalkar <147773549+nikhilc52@users.noreply.github.com>
2024-06-25 15:05:36 -0400

    Update working.R

--- advanced_animations/word_cloud/working.R
+++ advanced_animations/word_cloud/working.R
@@ -1,67 +1,50 @@
 library(plotly)
-library(tidyverse)
 library(dplyr)
-library(htmltools)
 library(stars)
-library(ggplot2)
-
-earthquakes <- read_csv("earthquakes.csv")
+library(tidyr)
+library(lubridate)
 
 degrees2radians <- function(degree) degree * pi / 180 
 
-earthquakes <- earthquakes |> 
-  mutate(x=1.01 * cos(degrees2radians(longitude)) * cos(degrees2radians(latitude))) |> 
-  mutate(y=1.01 * sin(degrees2radians(longitude)) * cos(degrees2radians(latitude))) |> 
-  mutate(z=1.01 * sin(degrees2radians(latitude)))
+shearwaters <- read.csv("occurrence.csv")
 
-x_size <- 1440
-y_size <- 720
-raw_tif <- read_stars("surface.png",
-                      RasterIO = list(nBufXSize=x_size, nBufYSize=y_size))
+shearwaters <- shearwaters[,names(shearwaters) %in% c("organismID", "verbatimEventDate", "decimalLatitude","decimalLongitude")]
 
-df_tif <- as.data.frame(raw_tif)
-df_tif <- df_tif |>
-  mutate(x = x-180) |>
-  mutate(y = y-90)
+shearwaters <- shearwaters |> 
+  separate_rows(organismID, sep=";") |> 
+  mutate(date = as.POSIXct(verbatimEventDate))
 
-red <- df_tif |> 
-  filter(band == 1) |> 
-  mutate(red = surface.png)
-red <- red[-c(3,4)]
+shearwaters <- shearwaters |> 
+  mutate(x=1.01 * cos(degrees2radians(decimalLongitude)) * cos(degrees2radians(decimalLatitude))) |> 
+  mutate(y=1.01 * sin(degrees2radians(decimalLongitude)) * cos(degrees2radians(decimalLatitude))) |> 
+  mutate(z=1.01 * sin(degrees2radians(decimalLatitude)))
 
-green <- df_tif |> 
-  filter(band == 2) |> 
-  mutate(green = surface.png)
-green <- green[-c(3,4)]
+time_range <- seq(
+  from = as.Date(min(shearwaters$date)),
+  to = as.Date(max(shearwaters$date)),
+  by = "day"
+)
 
-blue <- df_tif |> 
-  filter(band == 3) |> 
-  mutate(blue = surface.png)
-blue <- blue[-c(3,4)]
+animals <- unique(shearwaters$organismID)
+full_times <- expand.grid(Animal = animals, Time = time_range)
 
-rgb <- left_join(left_join(red, green),blue)
-rgb$color <- rgb(rgb$red/255,rgb$green/255,rgb$blue/255)
-rgb$color_int <- bitwShiftL(rgb$red, 16) + bitwShiftL(rgb$green, 8) + rgb$blue 
+shearwaters_full <- full_times |>
+  left_join(shearwaters, by = c("Animal" = "organismID"))|> 
+  group_by(Animal, Time) |> 
+  filter(abs(difftime(Time, date, units = "secs")) == min(abs(difftime(Time, date, units = "secs")))) |>
+  filter(abs(difftime(Time, date, units = "days")) < 1) |> 
+  ungroup() |>
+  select(Animal, Time, date, decimalLatitude, decimalLongitude, x, y, z) |> 
+  group_by(Time) |> 
+  filter(n() > 3) |> 
+  filter(Time >= as.Date("2010-01-01"))
 
-rgb_earth <- matrix(data=rgb$color_int, nrow=y_size, ncol = x_size, byrow=TRUE)
+shearwaters_full$Time <- as.character(shearwaters_full$Time)
 
-earth_colorscale <- distinct(data.frame(rgb$color_int, rgb$color))
-earth_colorscale <- earth_colorscale |> arrange(rgb.color_int)
+##########
+x_size <- 1000
+y_size <- 500
 
-while(nrow(earth_colorscale) > 255){
-  toDelete <- seq(0, nrow(earth_colorscale), 2)
-  earth_colorscale <- earth_colorscale[toDelete, ]
-  rownames(earth_colorscale) = NULL
-}
-
-earth_colorscale$breaks <- seq(1:nrow(earth_colorscale))/nrow(earth_colorscale)
-earth_colorscale$breaks[1] = 0
-earth_colorscale <- earth_colorscale[,c(3,2)]
-names(earth_colorscale)[names(earth_colorscale) == 'rgb.color'] <- 'colors'
-
-manual_colorscale <- list(c(0,0.2,0.4,0.6,0.8,1),
-                          c("white","#ffa590","#ff8164","#ff6242",
-                            "#fb4b1e","#c61a09")) 
 empty_axis <- list(
   showgrid = FALSE, 
   zeroline = FALSE,
@@ -71,75 +54,151 @@ empty_axis <- list(
   title = ""
 )
 
-nlat <- y_size
-nlon <- x_size
-lat <- seq(-90, 90, length.out = nlat)
-lon <- seq(-180, 180, length.out = nlon)
-lat <- matrix(rep(lat, nlon), nrow = nlat)
-lon <- matrix(rep(lon, each = nlat), nrow = nlat)
+lat <- seq(-90, 90, length.out = y_size)
+lon <- seq(-180, 180, length.out = x_size)
+lat <- matrix(rep(lat, x_size), nrow = y_size)
+lon <- matrix(rep(lon, each = y_size), nrow = y_size)
 
-image <- RCurl::base64Encode(readBin("size_scale.png", "raw", file.info("size_scale.png")[1, "size"]), "txt")
+average_positions <- shearwaters_full |> 
+  group_by(Time) |> 
+  summarize(
+    average_x = mean(x),
+    average_y = mean(y),
+    average_z = mean(z)
+  )
 
-globe <- plot_ly(width=800,height=800) |> 
-  add_sf(
-    data = sf::st_as_sf(maps::map("world", plot = FALSE, fill = TRUE)),
-    x = ~ 1.001 * cos(degrees2radians(x)) * cos(degrees2radians(y)),
-    y = ~ 1.001 * sin(degrees2radians(x)) * cos(degrees2radians(y)),
-    z = ~ 1.001 * sin(degrees2radians(y)),
-    color = I("black"), size = I(1),
-    hoverinfo = "none"
-  ) |>
-  add_trace(
-    x=earthquakes$x,
-    y=earthquakes$y,
-    z=earthquakes$z,
-    mode = "markers", type = "scatter3d",
-    marker = list(color = earthquakes$magnitude, size = earthquakes$sig/100, colorscale = manual_colorscale, showscale=TRUE,
-                  colorbar=list(title=list(text="Magnitude",side="top"), thickness=10, len=0.35,orientation='h',y=0.1,
-                                tickfont=list(family="Arial"), nticks=6)),
-    text = paste0("Description: ", earthquakes$title, "<br>", "Time: ", earthquakes$date_time, "<br>" ,"Significance: ", earthquakes$sig),
-    hoverinfo = "text"
-  ) |>
-  add_surface(
-    x = cos(degrees2radians(lon)) * cos(degrees2radians(lat)),
-    y = cos(degrees2radians(lat)) * sin(degrees2radians(lon)),
-    z = -sin(degrees2radians(lat)),
-    surfacecolor=rgb_earth,
-    colorscale=earth_colorscale,
-    showscale = FALSE, 
-    hoverinfo = "none",
-    lightposition = list(
-      x=1,
-      y=1,
-      z=0.5
-    ),
-    contours = list(
-      x = list(highlight = FALSE), 
-      y = list(highlight = FALSE), 
-      z = list(highlight = FALSE)
+
+spline_fit <- smooth.spline(average_positions$average_x, spar = .8)
+camera_x_positions <- predict(spline_fit)$y
+spline_fit <- smooth.spline(average_positions$average_y, spar = .8)
+camera_y_positions <- predict(spline_fit)$y
+spline_fit <- smooth.spline(average_positions$average_z, spar = .8)
+camera_z_positions <- predict(spline_fit)$y
+
+#if (!require("processx")) install.packages("processx")
+
+###########
+generate_surface <- function(file){
+  raw_tif <- read_stars(paste0("surfaces/", file, ".png"), RasterIO = list(nBufXSize=x_size, nBufYSize=y_size))
+  
+  df_tif <- as.data.frame(raw_tif)
+  df_tif <- df_tif |>
+    mutate(x = x-180) |>
+    mutate(y = y-90)
+  
+  red <- df_tif |> 
+    filter(band == 1)
+  names(red)[names(red) == paste0("X",file,".png")] <- 'red'
+  red <- red[-3]
+  
+  green <- df_tif |> 
+    filter(band == 2)
+  names(green)[names(green) == paste0("X",file,".png")] <- 'green'
+  green <- green[-3]
+  
+  blue <- df_tif |> 
+    filter(band == 3)
+  names(blue)[names(blue) == paste0("X",file,".png")] <- 'blue'
+  blue <- blue[-3]
+  
+  rgb <- left_join(left_join(red, green),blue)
+  rgb$color <- rgb(rgb$red/255,rgb$green/255,rgb$blue/255)
+  rgb$color_int <- bitwShiftL(rgb$red, 16) + bitwShiftL(rgb$green, 8) + rgb$blue 
+  
+  rgb_earth <<- matrix(data=rgb$color_int, nrow=y_size, ncol = x_size, byrow=TRUE)
+  
+  earth_colorscale <- distinct(data.frame(rgb$color_int, rgb$color))
+  earth_colorscale <- earth_colorscale |> arrange(rgb.color_int)
+  
+  while(nrow(earth_colorscale) > 255){
+    toDelete <- seq(0, nrow(earth_colorscale), 2)
+    earth_colorscale <- earth_colorscale[toDelete, ]
+    rownames(earth_colorscale) = NULL
+  }
+  
+  earth_colorscale$breaks <- seq(1:nrow(earth_colorscale))/nrow(earth_colorscale)
+  earth_colorscale$breaks[1] = 0
+  earth_colorscale <- earth_colorscale[,c(3,2)]
+  names(earth_colorscale)[names(earth_colorscale) == 'rgb.color'] <- 'colors'
+  earth_colorscale <<- earth_colorscale
+}
+
+generate_globe <- function(curr_shearwaters, camera_index){
+  globe <<- curr_shearwaters |>  
+    plot_ly(height = 600) |> 
+    add_sf(
+      data = sf::st_as_sf(maps::map("world", plot = FALSE, fill = TRUE)), 
+      x = ~ 1.001 * cos(degrees2radians(x)) * cos(degrees2radians(y)),
+      y = ~ 1.001 * sin(degrees2radians(x)) * cos(degrees2radians(y)),
+      z = ~ 1.001 * sin(degrees2radians(y)),
+      color = I("black"), size = I(1),
+      hoverinfo = "none"
+    ) |> 
+    add_trace(
+      x=~curr_shearwaters$x,
+      y=~curr_shearwaters$y,
+      z=~curr_shearwaters$z,
+      mode = "markers", type = "scatter3d",
+      marker = list(size = 7),
+      hoverinfo = "none"
+    ) |> 
+    add_surface(
+      x = cos(degrees2radians(lon)) * cos(degrees2radians(lat)),
+      y = sin(degrees2radians(lon)) * cos(degrees2radians(lat)),
+      z = -sin(degrees2radians(lat)),
+      surfacecolor = rgb_earth,
+      colorscale=earth_colorscale,
+      showscale = FALSE, hoverinfo = "none",
+      lightposition = list(
+        x=2,
+        y=2,
+        z=2
+      ),
+      contours = list(
+        x = list(highlight = FALSE), 
+        y = list(highlight = FALSE), 
+        z = list(highlight = FALSE)
+      )
+    ) |> 
+    layout(
+      showlegend = FALSE,
+      annotations=list(list(text=paste0("Day: ", dates[i], "<br>Nikhil Chinchalkar For Princeton University | Migration and foraging ecology of Greater Shearwater | 2024"),
+                            showarrow=FALSE, font=list(family="Arial", size=28), y=0, bgcolor="white", opacity=0.85), 
+                       list(text="<br><b>Greater Shearwater Migration</b>",font=list(family="Arial", size=48), bgcolor="white", opacity=0.85, y=.9)),
+      scene = list(
+        xaxis = empty_axis,
+        yaxis = empty_axis,
+        zaxis = empty_axis,
+        aspectratio = list(x = 1, y = 1, z = 1),
+        camera = list(eye=list(x=camera_x_positions[camera_index]*2,
+                               y=camera_y_positions[camera_index],
+                               z=camera_z_positions[camera_index]))
+      )
     )
-  ) |>
-  layout(
-    title=list(text="<br><b>Earthquakes Since 1995</b>",font=list(family="Arial", size=30)),
-    showlegend = FALSE,
-    annotations=list(text="Nikhil Chinchalkar For Princeton University | Seismic Research Dataset | 2024",
-                     showarrow=FALSE, font=list(family="Arial", size=14), y=0),
-    images=list(list(source=paste('data:image/png;base64', image, sep=','),
-                     xref="paper",
-                     yref="paper",
-                     x=0.5,
-                     y=0.1,
-                     sizex=.25,
-                     sizey=.25,
-                     xanchor="center",
-                     yanchor="center")),
-    scene = list(
-      xaxis = empty_axis,
-      yaxis = empty_axis,
-      zaxis = empty_axis,
-      aspectratio = list(x = 1, y = 1, z = 1)
-    )
-  ) |> 
-  config(displayModeBar=FALSE)
+}
 
-globe
+dates <- unique(shearwaters_full$Time)
+image_index <- 1
+system.time(
+  for(i in 1:length(dates)){
+    print(paste("Date:",dates[i]))
+    print(paste("Frame #:", image_index))
+    
+    file = formatC(yday(as.Date(dates[i])), width = 3, format = "d", flag = "0")
+    generate_surface(file)
+    
+    curr_shearwaters <- shearwaters_full |> 
+      filter(Time == dates[i])
+    
+    generate_globe(curr_shearwaters, i)
+    
+    orca(globe, paste0("image_sequence/",formatC(image_index, width = 3, format = "d", flag = "0"),".png"), width = 7*200, height = 7*200)
+    image_index <- image_index + 1
+  }
+)
+
+png_files <- sort(list.files("image_sequence", pattern = "*.png", full.names = TRUE))
+for(i in 1:length(png_files)/6){
+  png_files <- append(png_files, png_files[length(png_files)])
+}
+gifski::gifski(png_files, gif_file = "final.gif", width = 7*200, height = 7*200, delay = 30/length(png_files))
